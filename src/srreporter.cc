b6bad2ea044c57cbda34d5876debad3d41ed14e6
Tiensbakung <Tiensbakung@gmail.com>
2016-02-11 22:19:06 +0100

    [fix]reporter: request out of order in aggregation when buffering is used

--- src/srreporter.cc
+++ src/srreporter.cc
@@ -1,29 +1,19 @@
 #include <unistd.h>
 #include <cstring>
 #include "srreporter.h"
+#ifndef SR_REPORTER_VAL
+#define SR_REPORTER_VAL 400
+#endif
+#ifndef SR_REPORTER_NUM
+#define SR_REPORTER_NUM 32
+#endif
 using namespace std;
 
 
-static void _insert(deque<string> &d, uint16_t cap, string &buf,
-                    const SrQueue<SrNews>::Event &e)
+static void _insert(deque<string> &d, uint16_t cap, const string &s)
 {
-        if (e.first.prio & 1) {
-                if (d.size() >= cap)
-                        d.pop_front();
-                d.push_back(e.first.data + "\n");
-        } else {
-                buf += e.first.data + "\n";
-        }
-}
-
-
-static string _pack(const deque<string> &d, const string &buf)
-{
-        string s;
-        typedef deque<string>::const_iterator myiter;
-        for (myiter iter = d.begin(); iter != d.end(); ++iter)
-                s += *iter;
-        return s + buf;
+        if (d.size() >= cap) d.pop_front();
+        d.emplace_back(s);
 }
 
 
@@ -43,23 +33,34 @@ void* SrReporter::func(void *arg)
 {
         SrReporter *rep = (SrReporter*)arg;
         rep->http.setTimeout(20);
-        string aggre;
+        string s;
         const string selector = "87,," + rep->xid + "\n";
+        vector<string> vec;
         while (true) {
-                aggre.clear();
+                s.clear();
                 SrQueue<SrNews>::Event e = rep->out.get();
-                if (e.second != SrQueue<SrNews>::Q_OK)
-                        continue;
-                _insert(rep->buffer, rep->_cap, aggre, e);
+                if (e.second) continue;
 
-                e = rep->out.get(400);
-                for (uint8_t i = 0; e.second == SrQueue<SrNews>::Q_OK && i < 40;
-                     ++i, e = rep->out.get(400))
-                        _insert(rep->buffer, rep->_cap, aggre, e);
-                if (aggre.empty() || rep->sleeping)
+                if (!rep->sleeping) {
+                        for (auto &item: rep->buffer) s += item + "\n";
+                }
+                s += e.first.data + "\n";
+                if (e.first.prio & 1)
+                        _insert(rep->buffer, rep->_cap, e.first.data);
+
+                // request aggregation
+                for (int j = 1; j < SR_REPORTER_NUM; ++j) {
+                        e = rep->out.get(SR_REPORTER_VAL);
+                        if (e.second) break;
+
+                        s += e.first.data + "\n";
+                        if (e.first.prio & 1)
+                                _insert(rep->buffer, rep->_cap, e.first.data);
+                }
+                if (rep->sleeping)
                         continue;
 
-                const string s = _pack(rep->buffer, aggre);
+                // exponential waiting
                 for (uint16_t i = 2; i & 1023; i <<= 1) {
                         if (rep->http.post(s) >= 0) {
                                 rep->buffer.clear();
