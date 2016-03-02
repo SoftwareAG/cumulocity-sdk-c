55a5e8de28fcc4ab436974acf6c592cc16818334
Tiensbakung <Tiensbakung@gmail.com>
2016-03-02 15:42:44 +0100

    reporter: re-factoring: more compact loop for reqeust aggreation and exponential waiting in retries

--- src/srreporter.cc
+++ src/srreporter.cc
@@ -10,6 +10,7 @@
 #ifndef SR_REPORTER_RETRIES
 #define SR_REPORTER_RETRIES 10
 #endif
+#define Q_OK SrQueue<SrNews>::Q_OK
 using namespace std;
 
 
@@ -41,41 +42,35 @@ void* SrReporter::func(void *arg)
         vector<string> vec;
         while (true) {
                 s.clear();
-                SrQueue<SrNews>::Event e = rep->out.get();
-                if (e.second) continue;
+                auto e = rep->out.get();
+                if (e.second != Q_OK) continue;
 
                 if (!rep->sleeping) {
-                        for (auto &item: rep->buffer) s += item + "\n";
+                        for (const auto &item: rep->buffer) s += item + "\n";
                 }
-                s += e.first.data + "\n";
-                if (e.first.prio & 1)
-                        _insert(rep->buffer, rep->_cap, e.first.data);
-
                 // request aggregation
-                for (int j = 1; j < SR_REPORTER_NUM; ++j) {
-                        e = rep->out.get(SR_REPORTER_VAL);
-                        if (e.second) break;
-
-                        s += e.first.data + "\n";
+                for (int j = 1; e.second == Q_OK && j < SR_REPORTER_NUM;
+                     ++j, e = rep->out.get(SR_REPORTER_VAL)) {
+                        const string &data = e.first.data;
+                        s += data + "\n";
                         if (e.first.prio & 1)
-                                _insert(rep->buffer, rep->_cap, e.first.data);
+                                _insert(rep->buffer, rep->_cap, data);
                 }
-                if (rep->sleeping)
-                        continue;
+                if (rep->sleeping) continue;
 
                 // exponential waiting
-                for (uint32_t i = 0; i < SR_REPORTER_RETRIES; ++i) {
-                        if (rep->http.post(s) >= 0) {
-                                rep->buffer.clear();
-                                const string &resp = rep->http.response();
-                                if (!resp.empty()) {
-                                        rep->in.put(SrOpBatch(selector + resp));
-                                        rep->http.clear();
-                                }
-                                break;
-                        }
+                int c = rep->http.post(s);
+                for (uint32_t i = 0; c < 0 && i < SR_REPORTER_RETRIES;
+                     ++i, c = rep->http.post(s)) {
                         ::sleep(1 << i);
                 }
+                if (c >= 0) {
+                        rep->buffer.clear();
+                        const string &resp = rep->http.response();
+                        if (!resp.empty())
+                                rep->in.put(SrOpBatch(selector + resp));
+                }
+                rep->http.clear();
         }
         return NULL;
 }
