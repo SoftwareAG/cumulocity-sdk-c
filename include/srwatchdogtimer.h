88dadb04f132f635d90a70b5e20073fc22ea323d
Tiensbakung <Tiensbakung@gmail.com>
2016-03-10 12:34:57 +0100

    srwatchdogtimer: fix typo of SrWatchDogTimer to SrWatchdogTimer.

--- include/srwatchdogtimer.h
+++ include/srwatchdogtimer.h
@@ -13,17 +13,17 @@
  *  implementation. It must be used in couple with the srwatchdogd daemon for
  *  effect.
  */
-class SrWatchDogTimer
+class SrWatchdogTimer
 {
 public:
-        SrWatchDogTimer():
+        SrWatchdogTimer():
                 shm(NULL), shmid(shmget(getppid(), SR_WDT_SIZE, 0666)), c(0) {
                 shm = shmid != -1 ? (char*)shmat(shmid, NULL, 0) : shm;
                 shm = shm == (char*)-1 ? NULL : shm;
                 if (shm)
                         *shm = 1;
         }
-        virtual ~SrWatchDogTimer() {shmdt(shm);}
+        virtual ~SrWatchdogTimer() {shmdt(shm);}
         /**
          *  \brief Start the watchdog timer.
          *  \return 0 on success, -1 on failure.
